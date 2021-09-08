9c6259c45dc562cb0d2a4447779489b9a1d89c84
SAIMADHUYAL <saimadhu.yal@gmail.com>
2021-09-08 14:53:30 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+
+
+// function printName(){
+//     var name =  "Sai";
+//     console.log(name);
+// }
+// console.log(name);
+
+// printName();
+
+// Using let
+// var name = "Sai";
+// if(name == "Sai"){
+//     let fullName = "Sai Madhu";
+    
+// }
+// console.log(fullName);
+
+//using const
+// if a value doesnt need to be updated later in the block 'const' can be used.
+
+var name= 'h';
+if(name == 'h'){
+    let fullName = "Hello world";
+    const age = 21;
+
+}
+console.log(age);
