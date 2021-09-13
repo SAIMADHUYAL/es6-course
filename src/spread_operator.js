1c1ae0b0bcd15305f4332dde1b200c520d19085a
SAIMADHUYAL <saimadhu.yal@gmail.com>
2021-09-13 09:17:58 -0500

    Spread operator commit

--- src/spread_operator.js
+++ src/spread_operator.js
@@ -1,4 +1,14 @@
 const names = ['John','Bill','Sam'];
 const moreName = ['William','Paul','Mike'];
 
-console.log(...names);
+//console.log(...names);
+
+// let allNames = [];
+// allNames.allNames.concat(names);
+// allNames.push('Gio');
+// allNames = allNames.concat(moreNames);
+
+// console.log(allNames);
+
+const allNames = [...names,'Gio',...moreName];
+console.log(...allNames);
